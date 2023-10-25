d5875759a67c3cdbf692a75401d3c83e0d60f636
LevkoBe <lbeniakh@kse.org.ua>
2023-10-25 16:19:52 +0300

    simplified

--- src/components/calendar/Month.js
+++ src/components/calendar/Month.js
@@ -1,61 +1,52 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./Month.css";
 import DayCell from "./DayCell";
 
 const Month = ({ tasks }) => {
-  const [calendar, setCalendar] = useState([]);
+  const currentDate = new Date();
+  const currentMonth = currentDate.getMonth();
+  const currentYear = currentDate.getFullYear();
 
-  useEffect(() => {
-    const currentDate = new Date();
-    const currentMonth = currentDate.getMonth();
-    const currentYear = currentDate.getFullYear();
+  const firstDayOfMonth = new Date(currentYear, currentMonth, 1);
+  const prevMonthLastDate = new Date(currentYear, currentMonth, 0).getDate();
+  const firstDay = firstDayOfMonth.getDay(); // 0 - Sunday, 1 - Monday, ...
 
-    const firstDayOfMonth = new Date(currentYear, currentMonth, 1);
-    const lastDayOfMonth = new Date(currentYear, currentMonth + 1, 0);
-    const prevMonthLastDate = new Date(currentYear, currentMonth, 0).getDate();
+  const totalDays = new Date(currentYear, currentMonth + 1, 0).getDate();
+  const totalDaysInPrevMonth = firstDay === 0 ? 6 : firstDay - 1;
+  const nextMonthDays = 42 - (totalDays + totalDaysInPrevMonth);
 
-    const firstDay = firstDayOfMonth.getDay(); // 0 - Sunday, 1 - Monday, ...
+  const prevMonth = [];
+  const currentMonthDays = [];
+  const nextMonth = [];
 
-    const totalDays = lastDayOfMonth.getDate();
-    const totalDaysInPrevMonth = firstDay === 0 ? 6 : firstDay - 1;
+  for (let i = totalDaysInPrevMonth; i > 0; i--) {
+    const prevMonthDate = new Date(currentYear, currentMonth - 1, prevMonthLastDate - i + 1);
+    prevMonth.unshift({ day: prevMonthDate.toDateString(), isCurrentMonth: false });
+  }
 
-    const nextMonthDays = 42 - (totalDays + totalDaysInPrevMonth);
+  for (let i = 1; i <= totalDays; i++) {
+    const currentMonthDate = new Date(currentYear, currentMonth, i);
+    currentMonthDays.push({ day: currentMonthDate.toDateString(), isCurrentMonth: true, isToday: i === currentDate.getDate() });
+  }
 
-    const prevMonth = [];
-    const currentMonthDays = [];
-    const nextMonth = [];
+  for (let i = 1; i <= nextMonthDays; i++) {
+    const nextMonthDate = new Date(currentYear, currentMonth + 1, i);
+    nextMonth.push({ day: nextMonthDate.toDateString(), isCurrentMonth: false });
+  }
 
-    for (let i = totalDaysInPrevMonth; i > 0; i--) {
-      const prevMonthDate = new Date(currentYear, currentMonth - 1, prevMonthLastDate - i + 1);
-      prevMonth.unshift({ day: prevMonthDate.toDateString(), isCurrentMonth: false });
-    }
+  const calendarArray = [...prevMonth, ...currentMonthDays, ...nextMonth];
+  const calendarRows = [];
 
-    for (let i = 1; i <= totalDays; i++) {
-      const currentMonthDate = new Date(currentYear, currentMonth, i);
-      currentMonthDays.push({ day: currentMonthDate.toDateString(), isCurrentMonth: true, isToday: i === currentDate.getDate() });
-    }
-
-    for (let i = 1; i <= nextMonthDays; i++) {
-      const nextMonthDate = new Date(currentYear, currentMonth + 1, i);
-      nextMonth.push({ day: nextMonthDate.toDateString(), isCurrentMonth: false });
-    }
-
-    const calendarArray = [...prevMonth, ...currentMonthDays, ...nextMonth];
-    const calendarRows = [];
-
-    for (let i = 0; i < 6; i++) {
-      const week = calendarArray.slice(i * 7, (i + 1) * 7);
-      calendarRows.push(week);
-    }
-
-    setCalendar(calendarRows);
-  }, []);
+  for (let i = 0; i < 6; i++) {
+    const week = calendarArray.slice(i * 7, (i + 1) * 7);
+    calendarRows.push(week);
+  }
 
   const dayLabels = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
   return (
     <div className="month-container">
-      <h2>{new Date().toLocaleString("en-US", { month: "long" })}</h2>
+      <h2>{currentDate.toLocaleString("en-US", { month: "long" })}</h2>
       <table className="month-table">
         <thead>
           <tr>
@@ -67,7 +58,8 @@ const Month = ({ tasks }) => {
           </tr>
         </thead>
         <tbody>
-          {calendar.map((week, index) => (
+          {console.log(tasks)}
+          {calendarRows.map((week, index) => (
             <tr key={index}>
               {week.map((day, index) => (
                 <td key={index}>
